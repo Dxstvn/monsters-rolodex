3c65d7ec10cc4802e26d30d2714fa6bf958e57c9
Dxstvn <jasmindustin@gmail.com>
2020-08-19 23:36:46 -0400

    Updates

---
+++ precache-manifest.08e897964014318e6f7896043cdce1fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75745a8d3a210f1d8206b3dd17897741",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7148d3a5a8f03eab5885",
+    "url": "/monsters-rolodex/static/css/main.718de642.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7148d3a5a8f03eab5885",
+    "url": "/monsters-rolodex/static/js/main.c13034bc.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
